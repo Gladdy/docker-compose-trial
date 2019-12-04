ca8c9da9bcbe11950b1ff8df2f459d06c1e167dc
Martijn Bakker <gladdyu@gmail.com>
2019-12-04 00:13:21 +0000

    update config to read the env file

--- backend/quickshort/settings.py
+++ backend/quickshort/settings.py
@@ -19,6 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+for k,v in os.environ.items():
+    print(f"k={k} v={v}")
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET"]
 
